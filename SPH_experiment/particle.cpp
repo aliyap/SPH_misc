c3f79b18646ba9065a7c27d98b43093e8b28a635
Aliya Pazylbekova <apazylbe@uwaterloo.ca>
2015-08-20 00:58:54 +0200

    renamed projects

--- SPH_experiment/particle.cpp
+++
@@ -1,6 +0,0 @@
-#include "particle.h"
-
-Particle::Particle(float x, float y, float z, float r, float g, float b, float vx, float vy, float vz): x(x), y(y), z(z), vx(0), vy(vx), vz(vy), r(r), g(g), b(b), pressure(0.0f)
-{
-
-}
